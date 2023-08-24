7f6bfb3f39ccd2069d0a79cbd96b4ed3bcf89cec
yanziqi95 <82500723+yanziqi95@users.noreply.github.com>
2023-08-24 16:28:39 +0100

    Add files via upload

---
+++ cli_pack.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"log"
+)
+
+func (cli *CLI) pack(addr string) {
+	if !ValidateAddress(addr) {
+		log.Panic("ERROR: 发送地址非法")
+	}
+
+	bc := NewBlockchain() //打开数据库，读取区块链并构建区块链实例
+	defer bc.Db.Close()   //转账完毕，关闭数据库
+	//txs := conn_recv(addr)
+
+	//开启一个主进程处理交易数据
+	go func() {
+		txs := recv_tx(addr)
+		bc.MineBlock(txs)
+	}()
+
+	//开启一个进程处理评论数据
+	go func() {
+		recvReview()
+	}()
+
+	go func() {
+		checkReview()
+	}()
+	select {}
+	fmt.Println("成功打包交易")
+}
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+const dns = "3.10.214.121"
+const fullNode1 = "18.134.137.142"
+const fullNode2 = "13.40.136.108"
+const blockSize = 3
+
+func main() {
+	cli := CLI{}
+	cli.Run()
+}
